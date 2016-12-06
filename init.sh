e9f6edcad588bf1132e0da0cd247366ab903cb91
Ivan Voischev <voischev@yandex-team.ru>
2016-12-06 16:02:21 +0300

    add ackrc

--- init.sh
+++ init.sh
@@ -9,4 +9,5 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # Configs
 ln -s $DOTFILES/profile         $HOME/.profile
 ln -s $DOTFILES/vimrc           $HOME/.vimrc
+ln -s $DOTFILES/ackrc           $HOME/.ackrc
 ln -s $DOTFILES/gitignore       $HOME/.gitignore
