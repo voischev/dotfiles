db5070047283a5264148b10ee7e4c2b9cf23d694
Ivan Voischev <voischev@yandex-team.ru>
2021-06-07 20:17:47 +0300

    скрипты

---
+++ scripts/cmake.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Зависимости
+sudo apt-get install build-essential libssl-dev
+
+# Последняя версия https://cmake.org/download/
+wget https://github.com/Kitware/CMake/releases/download/v3.20.2/cmake-3.20.2.tar.gz
+tar -zxvf cmake-3.20.2.tar.gz
+cd cmake-3.20.2
+
+./bootstrap
+make
+sudo make install
+
+rm -rf cmake-3.20.2
