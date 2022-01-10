3b21c8fc48eb26b60205473e62efe43369be7fc9
Ivan Voischev <voischev@yandex-team.ru>
2022-01-10 23:26:45 +0300

    refactor

--- setup.sh
+++ setup.sh
@@ -5,18 +5,6 @@ DOTFILES=$HOME/.dotfiles
 git clone https://github.com/voischev/dotfiles.git $DOTFILES
 
 cd $DOTFILES
-git submodule update --remote --merge
-
-# Пример удаления модулей
-#git submodule deinit vim/pack/plugins/start/vim-airline
-#git rm vim/pack/plugins/start/vim-airline
-#rm -rf .git/modules/vim/pack/plugins/start/vim-airline
-#git commit
-
-# YouCompleteMe
-cd vim/pack/plugins/start/YouCompleteMe
-git submodule update --init --recursive
-./install.py --ts-completer
 
 cd $HOME
 rm -rf                          .vim
@@ -26,5 +14,3 @@ ln -s $DOTFILES/bash_profile    .bash_profile
 ln -s $DOTFILES/ackrc           .ackrc
 ln -s $DOTFILES/gitignore       .gitignore
 ln -s $DOTFILES/tmux            .tmux.conf
-ln -s $DOTFILES/ctags           .ctags
-
